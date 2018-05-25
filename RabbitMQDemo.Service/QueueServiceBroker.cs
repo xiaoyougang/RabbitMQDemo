57375f601f444d98eea9b9a426f66ad6e0751fae
xiaoyougang <x.gangle@gmail.com>
2018-05-25 09:55:06 +0800

    更新项目代码。

--- RabbitMQDemo.Service/QueueServiceBroker.cs
+++ RabbitMQDemo.Service/QueueServiceBroker.cs
@@ -9,9 +9,9 @@ namespace RabbitMQDemo.Service
     {
         static readonly string EXCHANGE_NAME1 = "";
 
-        static readonly string QUEUE_NAME1 = "queue1"; 
-        static readonly string QUEUE_NAME2 = "queue2"; 
-        static readonly string QUEUE_NAME3 = "queue3"; 
+        static readonly string QUEUE_NAME1 = "queue1";
+        static readonly string QUEUE_NAME2 = "queue2";
+        static readonly string QUEUE_NAME3 = "queue3";
 
         static readonly string RK_RMQ1 = "RMQ.ONE.DOLA";
         static readonly string RK_RMQ2 = "RMQ.TWO.DOLA";
@@ -19,13 +19,13 @@ namespace RabbitMQDemo.Service
 
         public static ConnectionFactory InitConnectionFactory()
         {
-            var uri = new Uri("amqp://127.0.0.1:5672/");  
-            var factory = new ConnectionFactory  
-            {  
-                UserName = "admin",  
-                Password = "admin888",  
-                RequestedHeartbeat = 0,  
-                Endpoint = new AmqpTcpEndpoint(uri)  
+            var uri = new Uri("amqp://127.0.0.1:5672/");
+            var factory = new ConnectionFactory
+            {
+                UserName = "admin",
+                Password = "admin888",
+                RequestedHeartbeat = 0,
+                Endpoint = new AmqpTcpEndpoint(uri)
             };
             return factory;
         }
@@ -33,15 +33,15 @@ namespace RabbitMQDemo.Service
         public static void DeclareRoute(IModel channel, string exchangeType)
         {
             //设置交换器的类型  
-            channel.ExchangeDeclare(EXCHANGE_NAME1, exchangeType);  
+            channel.ExchangeDeclare(EXCHANGE_NAME1, exchangeType);
             //声明一个队列，设置队列是否持久化，排他性，与自动删除  
-            channel.QueueDeclare(QUEUE_NAME1, true, false, false, null);  
-            channel.QueueDeclare(QUEUE_NAME2, true, false, false, null);  
-            channel.QueueDeclare(QUEUE_NAME3, true, false, false, null);  
+            channel.QueueDeclare(QUEUE_NAME1, true, false, false, null);
+            channel.QueueDeclare(QUEUE_NAME2, true, false, false, null);
+            channel.QueueDeclare(QUEUE_NAME3, true, false, false, null);
             //绑定消息队列，交换器，routingkey  
-            channel.QueueBind(QUEUE_NAME1, EXCHANGE_NAME1, RK_RMQ1);  
-            channel.QueueBind(QUEUE_NAME2, EXCHANGE_NAME1, RK_RMQ2);  
-            channel.QueueBind(QUEUE_NAME3, EXCHANGE_NAME1, RK_RMQ3);  
+            channel.QueueBind(QUEUE_NAME1, EXCHANGE_NAME1, RK_RMQ1);
+            channel.QueueBind(QUEUE_NAME2, EXCHANGE_NAME1, RK_RMQ2);
+            channel.QueueBind(QUEUE_NAME3, EXCHANGE_NAME1, RK_RMQ3);
         }
 
         public static string GetQueueName(string userName)
@@ -56,40 +56,40 @@ namespace RabbitMQDemo.Service
                 return string.Empty;
         }
 
-        public static void Produce(string message, string exchangeType)  
+        public static void Produce(string message, string exchangeType)
         {
-            try  
+            try
             {
-                var factory = InitConnectionFactory(); 
-                using (var connection = factory.CreateConnection())  
-                {  
-                    using (var channel = connection.CreateModel())  
+                var factory = InitConnectionFactory();
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
                     {
-                        DeclareRoute(channel, exchangeType); 
+                        DeclareRoute(channel, exchangeType);
 
-                        var properties = channel.CreateBasicProperties();  
+                        var properties = channel.CreateBasicProperties();
                         //队列持久化  
                         properties.Persistent = true;
-                        var bytes = Encoding.UTF8.GetBytes(message); 
+                        var bytes = Encoding.UTF8.GetBytes(message);
                         //转发消息业务规则
-                        var routingKey = ""; 
+                        var routingKey = "";
                         if (exchangeType == ExchangeType.Topic)
                         {
-                            routingKey = "RMQ.*.DOLAR"; 
+                            routingKey = "RMQ.*.DOLAR";
                         }
                         else if (exchangeType == ExchangeType.Direct)
                         {
                             routingKey = "RMQ.ONE.DOLAR";
                         }
                         //发送信息  
-                        channel.BasicPublish(EXCHANGE_NAME1, routingKey, properties, bytes);  
-                    } 
-                }  
-            }  
-            catch (Exception ex)  
-            {  
-                Console.WriteLine(ex.Message);  
-            }  
+                        channel.BasicPublish(EXCHANGE_NAME1, routingKey, properties, bytes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public static string Consume(string exchangeType, string userName)
@@ -101,7 +101,7 @@ namespace RabbitMQDemo.Service
                 {
                     using (var channel = connection.CreateModel())
                     {
-                        DeclareRoute(channel, exchangeType); 
+                        DeclareRoute(channel, exchangeType);
 
                         //定义这个队列的消费者  
                         QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
